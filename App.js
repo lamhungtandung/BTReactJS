2d1cb45a9519c191e64ced4d1120e25723b965a8
lamhungtandung <lamhungtandung@gmail.com>
2022-09-18 02:52:55 +0700

    dua2

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
+import Content from './component/BTReactJS1/Content';
+import Item from './component/BTReactJS1/Item';
+import Footer from './component/BTReactJS1/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Content/>
+      <Item/>
+      <Footer/>
     </div>
   );
 }
