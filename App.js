56d3c566213610a864fd04b911c10bf3f6237224
lamhungtandung <lamhungtandung@gmail.com>
2022-09-20 03:35:54 +0700

    duabt2

--- App.js
+++ App.js
@@ -4,14 +4,22 @@ import Header from './component/Header';
 import Content from './component/BTReactJS1/Content';
 import Item from './component/BTReactJS1/Item';
 import Footer from './component/BTReactJS1/Footer';
+import Glasses from './component/BTReactJS2/Glasses';
+import TitleGlasses from './component/BTReactJS2/TitleGlass';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* BT1 */}
+      {/* <Header/>
       <Content/>
       <Item/>
-      <Footer/>
+  <Footer/> */}
+
+      {/* BT2
+      */}
+      <TitleGlasses/>
+      <Glasses/>
     </div>
   );
 }
